716e1adf5972a417ff39394be4ea539ba9e26cfa
Donald Piret <donald@donaldpiret.com>
2014-05-28 18:00:15 +0800

    Add node shims to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:$HOME/bin:/opt/boxen/homebrew/bin:/usr/local/share/python:$DOTFILES/bin
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:$HOME/bin:/opt/boxen/homebrew/bin:/opt/boxen/nodenv/shims:/usr/local/share/python:$DOTFILES/bin
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
